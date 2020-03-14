f577753c99e7bd8c19d0e429015caa28d11fcd35
Mehran <mehran.git@hotmail.com>
2020-03-14 17:34:03 -0400

    Create Hangeul class and calculate ci, vi, di for  create Hangeul's object

---
+++ Coreen/Hangeul.java
@@ -0,0 +1,25 @@
+package Coreen;
+
+public class Hangeul {
+    private int ci;
+    private int vi;
+    private int di;
+
+    public Hangeul(int ci, int vi, int di) {
+        this.ci = ci;
+        this.vi = vi;
+        this.di = di;
+    }
+
+    public int getCi() {
+        return ci;
+    }
+
+    public int getVi() {
+        return vi;
+    }
+
+    public int getDi() {
+        return di;
+    }
+}
--- Coreen/TexteCoreen.java
+++ Coreen/TexteCoreen.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class TexteCoreen {
     String texte;
     ArrayList<Integer> codePoints = new ArrayList<>();
+    ArrayList<Hangeul> hangeuls = new ArrayList<>();
 
     public TexteCoreen(String texte) {
         this.texte = texte;
@@ -22,17 +23,42 @@ public class TexteCoreen {
         return 0;
     }
 
-    public void transferAuCodesPoints(){
+    /**
+     * Calculer code point Correspondant de chaque caractère.
+     *
+     * @return void
+     */
+    private void transferAuCodesPoints() {
         int codePoint;
-        for (int i = 0; i < texte.length(); i++){
+        for(int i = 0; i < texte.length(); i++) {
             codePoint = texte.codePointAt(i);
             codePoints.add(codePoint);
         }
     }
 
+    /**
+     * Calculer ci, vi, di et créer object Hangeul de chaque code point.
+     *
+     * @return void
+     */
+    private void calculerHangeul() {
+        int uiTemp;
+        int ci;
+        int vi;
+        int di;
+        for(int codePoint : codePoints) {
+            uiTemp = codePoint - 44032;
+            di = uiTemp % 28;
+            vi = 1 + ((uiTemp - di) % 588) / 28;
+            ci = 1 + (uiTemp / 588);
+            Hangeul h = new Hangeul(ci, vi, di);
+            hangeuls.add(h);
+        }
+    }
+
     public String traduire() {
         transferAuCodesPoints();
-        return codePoints.toString();
+        calculerHangeul();
         //return texte;
     }
 }

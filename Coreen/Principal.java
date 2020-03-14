033906c4b36181c38d1ca2c17aa39193e9c620ef
Mehran <mehran.git@hotmail.com>
2020-03-14 16:50:06 -0400

    step one - calculate codes point

--- Coreen/Principal.java
+++ Coreen/Principal.java
@@ -2,6 +2,7 @@ package Coreen;
 
 import javax.swing.SwingUtilities;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Principal {
     public static void main(String arg[]) {
--- Coreen/TexteCoreen.java
+++ Coreen/TexteCoreen.java
@@ -1,7 +1,10 @@
 package Coreen;
 
+import java.util.ArrayList;
+
 public class TexteCoreen {
     String texte;
+    ArrayList<Integer> codePoints = new ArrayList<>();
 
     public TexteCoreen(String texte) {
         this.texte = texte;
@@ -19,7 +22,17 @@ public class TexteCoreen {
         return 0;
     }
 
+    public void transferAuCodesPoints(){
+        int codePoint;
+        for (int i = 0; i < texte.length(); i++){
+            codePoint = texte.codePointAt(i);
+            codePoints.add(codePoint);
+        }
+    }
+
     public String traduire() {
-        return texte;
+        transferAuCodesPoints();
+        return codePoints.toString();
+        //return texte;
     }
 }
